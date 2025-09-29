d13e04aaec874fbe0fe6357499ec251422942da5
Liujinxin1111 <315033954@qq.com>
2025-09-29 09:26:04 +0800

    Delete input files directory

--- input files/ASGB/get_file.sh
+++
@@ -1,4 +0,0 @@
-ln -s ./com.pdb
-ln -s ./pro.pdb
-ln -s ./lig.pdb
-ln -s ../../image.nc
--- input files/analysis scripts/run.sh
+++
@@ -1,3 +0,0 @@
-cpptraj ../com.top  < corre.in > m4.out 
-sed -i '/^[  ]*$/d' p-d.dat
-./trcomp_matrix_xyz.pl p-d.dat > dccmn425wt.dat
